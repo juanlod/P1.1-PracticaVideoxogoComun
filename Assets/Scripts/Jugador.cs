639e9d3ea92b14f30fb7f625b2c3a60b4321ed28
BorjaFdzAn <BorjaFdzAn@gmail.com>
2021-12-21 19:46:15 +0100

    Movimiento lateral izquierdo con respecto a la issue #1

--- Assets/Scripts/Jugador.cs
+++ Assets/Scripts/Jugador.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Jugador : MonoBehaviour
 {
     // Start is called before the first frame update
+    public float VELOCIDAD = 5;
+    float direccion;
     void Start()
     {
         
@@ -13,6 +15,16 @@ public class Jugador : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        MoverJugador(); 
+    }
+
+    private void MoverJugador(){
+        direccion = 0;
+        Vector2 posicionSiguiente = transform.position;
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)){
+            direccion = -1;
+        }
+        posicionSiguiente.x += direccion * VELOCIDAD * Time.deltaTime;
+        transform.position = posicionSiguiente;
     }
 }
