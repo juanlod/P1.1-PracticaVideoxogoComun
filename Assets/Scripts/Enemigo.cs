f9453c140246c94229f4e388a0cd4adf2896e93e
BorjaFdzAn <borjafdzan@gmail.com>
2021-12-23 13:10:55 +0100

    Add comentarios para hacer que el codigo sea mas legible

--- Assets/Scripts/Enemigo.cs
+++ Assets/Scripts/Enemigo.cs
@@ -24,15 +24,24 @@ public class Enemigo : MonoBehaviour
         contador -= Time.deltaTime;
     }
 
+
+    //La funcion mueve al personaje dependiendo de la posicion y la direccion 
+    //tambien rota el sprite dependiendo de la direccion
     private void MoverEnemigo(){
         Vector3 escala = transform.localScale;
         Vector2 posicion = transform.position;
+
+        //Se mueve el personaje
         posicion.x += VELOCIDAD * direccion * Time.deltaTime;
+        //Se cambia de direccion el sprite
         escala.x = Mathf.Abs(escala.x) * direccion;
+
         transform.position = posicion;
         transform.localScale = escala;
     }
 
+    //La funcion CambiarDireccion cambia la direccion del personaje y 
+    //asigna un numero aleatoria para el proximo cambio de direccion
     private void CambiarDireccion(){
         direccion *= -1;
         contador = Random.Range(0.5f, 2.5f);
