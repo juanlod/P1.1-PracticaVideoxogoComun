df96e4730bdb044849d78f4c0b9b97d2de47399c
BorjaFdzAn <borjafdzan@gmail.com>
2021-12-24 18:31:42 +0100

    Inicializacion de la colision con el enemigo comprobando que choca con el jugador para resolver la issue #25

---
+++ Assets/Scripts/MuerteJugador.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuerteJugador : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D colision){
+        Jugador jugador = colision.gameObject.GetComponent<Jugador>();
+
+        if (jugador != null){
+            Debug.Log("El jugador ha chocado");
+        }
+    }
+}
