3d088172bffd30da7ae07ca120e3e90e8c814a01
BorjaFdzAn <borjafdzan@gmail.com>
2021-12-24 18:44:53 +0100

    Ajustar el respawn del jugador tambien cuando se caiga y se salga fuera del mapa para la issue #25

--- Assets/Scripts/MuerteJugador.cs
+++ Assets/Scripts/MuerteJugador.cs
@@ -19,7 +19,15 @@ public class MuerteJugador : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D colision){
         Jugador jugador = colision.gameObject.GetComponent<Jugador>();
+        RespawnJugador(jugador);
+    }
 
+    private void OnTriggerEnter2D(Collider2D colider){
+        Jugador jugador = colider.gameObject.GetComponent<Jugador>();
+        RespawnJugador(jugador);
+    }
+
+    private void RespawnJugador(Jugador jugador){
         if (jugador != null){
             jugador.Morir();
         }
