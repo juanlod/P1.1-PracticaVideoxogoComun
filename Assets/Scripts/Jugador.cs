b958e299d85b794c360d747452953d7f6c7c79da
BorjaFdzAn <borjafdzan@gmail.com>
2021-12-25 18:22:07 +0100

    Puesta la transicion al estado idle y el estado corriendo cuando aterriza el personaje para resolver la issue #12

--- Assets/Scripts/Jugador.cs
+++ Assets/Scripts/Jugador.cs
@@ -4,82 +4,102 @@ using UnityEngine;
 
 public class Jugador : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public float VELOCIDAD = 5;
+	// Start is called before the first frame update
+	public float VELOCIDAD = 5;
 
-    public float FUERZA_SALTO = 10;
+	public float FUERZA_SALTO = 10;
 
-    public LayerMask mascaraSuelo;
-    float direccion;
+	public LayerMask mascaraSuelo;
+	float direccion;
 
-    private Rigidbody2D cuerpo;
-    private Animator animator;
+	private Rigidbody2D cuerpo;
+	private Animator animator;
 
-    private CapsuleCollider2D colisionador;
+	private CapsuleCollider2D colisionador;
 
 
-    void Start()
-    {
-         this.cuerpo = GetComponent<Rigidbody2D>(); 
-         this.colisionador = GetComponent<CapsuleCollider2D>();
-         this.animator = GetComponent<Animator>();
-    }
+	void Start()
+	{
+		this.cuerpo = GetComponent<Rigidbody2D>();
+		this.colisionador = GetComponent<CapsuleCollider2D>();
+		this.animator = GetComponent<Animator>();
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        MoverJugador(); 
-        RotarSprite();
+	// Update is called once per frame
+	void Update()
+	{
+		bool isSuelo = ComprobarSuelo();
+		if (cuerpo.velocity.y < 0)
+		{
+			animator.SetBool("isCayendo", true);
+			animator.SetBool("isSaltando", false);
+		}
+		if (isSuelo)
+		{
+			animator.SetBool("isCayendo", false);
+			animator.SetBool("isSaltando", false);
+		}
+		MoverJugador();
+		RotarSprite();
 
-        bool isSuelo = ComprobarSuelo();
-        if (Input.GetKey(KeyCode.Space) && isSuelo){
-           Saltar();
-       }
+		if (Input.GetKey(KeyCode.Space) && isSuelo)
+		{
+			Saltar();
+		}
 
-       if (cuerpo.velocity.y < 0){
-           animator.SetBool("isCayendo", true);
-           animator.SetBool("isSaltando", false);
-       }
-    }
+	}
 
-    private void MoverJugador(){
-        direccion = 0;
-        Vector2 posicionSiguiente = transform.position;
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)){
-            direccion = -1;
-            animator.SetBool("isCorriendo", true);
-        } else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)){
-            direccion = 1;
-            animator.SetBool("isCorriendo", true);
-        } else {
-            animator.SetBool("isCorriendo", false);
-        }
+	private void MoverJugador()
+	{
+		direccion = 0;
+		Vector2 posicionSiguiente = transform.position;
+		if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+		{
+			direccion = -1;
+			animator.SetBool("isCorriendo", true);
+		}
+		else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+		{
+			direccion = 1;
+			animator.SetBool("isCorriendo", true);
+		}
+		else
+		{
+			animator.SetBool("isCorriendo", false);
+		}
 
-        posicionSiguiente.x += direccion * VELOCIDAD * Time.deltaTime;
-        transform.position = posicionSiguiente;
-    }
-    private void RotarSprite(){
-        Vector3 escalaLocal = transform.localScale;
-        if (direccion == -1){
-            escalaLocal.x = Mathf.Abs(escalaLocal.x) * -1;
-        } else if (direccion == 1){
-            escalaLocal.x = Mathf.Abs(escalaLocal.x);
-        }
-        transform.localScale = escalaLocal;
-    }
+		posicionSiguiente.x += direccion * VELOCIDAD * Time.deltaTime;
+		transform.position = posicionSiguiente;
+	}
+	private void RotarSprite()
+	{
+		Vector3 escalaLocal = transform.localScale;
+		if (direccion == -1)
+		{
+			escalaLocal.x = Mathf.Abs(escalaLocal.x) * -1;
+		}
+		else if (direccion == 1)
+		{
+			escalaLocal.x = Mathf.Abs(escalaLocal.x);
+		}
+		transform.localScale = escalaLocal;
+	}
 
-    private void Saltar(){
-        cuerpo.velocity = Vector2.up * FUERZA_SALTO;
-        animator.SetBool("isSaltando", true);
-    }
+	private void Saltar()
+	{
+		cuerpo.velocity = Vector2.up * FUERZA_SALTO;
+		animator.SetBool("isSaltando", true);
+	}
 
-    private bool ComprobarSuelo(){
-        float desfase = 0.01f;
-        RaycastHit2D golpe  = Physics2D.Raycast(this.colisionador.bounds.center, Vector2.down, this.colisionador.bounds.extents.y + desfase , mascaraSuelo );
-        return golpe.collider != null;
-    }
+	private bool ComprobarSuelo()
+	{
+		float desfase = 0.01f;
+		RaycastHit2D golpe = Physics2D.Raycast(this.colisionador.bounds.center, Vector2.down, this.colisionador.bounds.extents.y + desfase, mascaraSuelo);
+		return golpe.collider != null;
+	}
 
-    public void Morir(){
-        this.transform.position = ControladorJuego.instancia.posicionRespawn.position;
-    }
+	public void Morir()
+	{
+		this.transform.position = ControladorJuego.instancia.posicionRespawn.position;
+	}
 }
